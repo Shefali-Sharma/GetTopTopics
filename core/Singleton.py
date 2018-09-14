e6418b6b823832bb1802cb6022a06358c5a12228
Shefali-Sharma <shefali92@gmail.com>
2018-09-14 17:11:42 -0400

    Adding a singleton base class
    Adding a singleton base class

---
+++ core/Singleton.py
@@ -0,0 +1,12 @@
+__author__ = 'shefali'
+
+class Singleton:
+    instance = None
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_instance(cls):
+        if cls.instance is None or not isinstance(cls.instance, cls):
+            cls.instance = cls()
+        return cls.instance
