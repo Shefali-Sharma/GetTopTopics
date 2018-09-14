f2666bb6e7495049afd5b01e52ebd6a20ee2c666
Shefali-Sharma <shefali92@gmail.com>
2018-09-14 14:51:07 -0400

    Code Refactoring
    Code Refactoring

--- app.py
+++ app.py
@@ -8,20 +8,20 @@ Topics = []
 @app.route('/')
 def home():
     try:
-        return render_template('layout.html', topicsList = [])
+        return render_template('home.html', topicsList = [])
     except:
         print ('Error encountered for this data: ' + str(sys.exc_info()[0]))
-        return render_template('layout.html', topicsList = [])
+        return render_template('home.html', topicsList = [])
 
 @app.route('/callback', methods = ['POST'])
 def callback():
     try:
         with open("text.log",'w') as f:
             f.write(request.form['input'] + "\n")
-        return render_template('layout.html', topicsList = Topics)
+        return render_template('home.html', topicsList = Topics)
     except:
         print ('Error encountered for this data: ' + str(sys.exc_info()[0]))
-        return render_template('layout.html', topicsList = [])
+        return render_template('home.html', topicsList = [])
 
 @app.route('/data', methods = ['GET'])
 def getdata():
@@ -29,20 +29,20 @@ def getdata():
         with open("text.log",'r') as f:
             a = list(processing.top_keywords_from_url(f.readline().strip(),10).to_dict()["index"].values())
             if not a:
-                return render_template('layout.html', topicsList = [])
-        return render_template('layout.html', topicsList = a)
+                return render_template('home.html', topicsList = [])
+        return render_template('home.html', topicsList = a)
     except NameError as err:
         print ('NameError in this data: ' + str(err))
-        return render_template('layout.html', topicsList = [])
+        return render_template('home.html', topicsList = [])
     except TypeError as err:
         print ('TypeError in this data: ' + str(err))
-        return render_template('layout.html', topicsList = [])
+        return render_template('home.html', topicsList = [])
     except ValueError as err:
         print ('ValueError in this data: ' + str(err))
-        return render_template('layout.html', topicsList = [])
+        return render_template('home.html', topicsList = [])
     except:
         print ('Error encountered for this data: ' + str(sys.exc_info()[0]))
-        return render_template('layout.html', topicsList = [])
+        return render_template('home.html', topicsList = [])
 
 
 if __name__ == '__main__':
